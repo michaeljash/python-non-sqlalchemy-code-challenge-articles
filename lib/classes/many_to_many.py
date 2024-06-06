9ba0d9c70a91e672d31eda49feff099aa5d707a9
jashonmichael <jashon.michael@student.moringaschool.com>
2024-06-06 11:06:24 +0300

    Complete code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,147 +1,69 @@
 class Article:
     def __init__(self, author, magazine, title):
-        self._author = None
-        self._magazine = None
-        self.title = title
+        if not isinstance(title, str) or not 5 <= len(title) <= 50:
+            raise ValueError("Must be a string between 5 and 50 characters inclusive.")
+        if not isinstance(author, Author):
+            raise ValueError("Must be of type Author.")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Must be of type Magazine.")
         self.author = author
         self.magazine = magazine
-
-    @property
-    def title(self):
-        return self._title
-
-    @title.setter
-    def title(self, value):
-        if not isinstance(value, str) or not (5 <= len(value) <= 50):
-            raise ValueError("Title must be a string between 5 and 50 characters.")
-        if hasattr(self, '_title'):
-            raise AttributeError("Cannot modify the title after initialization.")
-        self._title = value
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, value):
-        if not isinstance(value, Author):
-            raise ValueError("Author must be an instance of Author.")
-        self._author = value
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, value):
-        if not isinstance(value, Magazine):
-            raise ValueError("Magazine must be an instance of Magazine.")
-        self._magazine = value
-
-
+        self.title = title
 class Author:
     def __init__(self, name):
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Must be a non-empty string.")
         self.name = name
-        self._articles = []
+        self.articles = set()
+        self.magazines = set()
 
-    @property
     def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise ValueError("Error")
-        if hasattr(self, '_name'):
-            raise AttributeError("Cannot modify the name after initialization.")
-        self._name = value
+        return self.name
 
     def articles(self):
-        return self._articles
+        return self.articles
 
     def magazines(self):
-        return list(set(article.magazine for article in self._articles))
+        return self.magazines
 
     def add_article(self, magazine, title):
-        new_article = Article(self, magazine, title)
-        self._articles.append(new_article)
-        magazine._articles.append(new_article)
-        return new_article
+        if magazine not in self.magazines:
+            raise ValueError('The specified magazine is not associated with this author.')
+        article = Article(self, magazine, title)
+        self.articles.append(article)
+        magazine.add_article(article)
 
     def topic_areas(self):
-        if not self._articles:
-            return None
-        return list(set(magazine.category for magazine in self.magazines()))
-
+        return set(article.topic_areas for article in self.articles)
 
 class Magazine:
-    _all_magazines = []
-
     def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters")
         self.name = name
-        self.category = category
-        self._articles = []
-        Magazine._all_magazines.append(self)
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str) or not (2 <= len(value) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters.")
-        self._name = value
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, value):
-        if not isinstance(value, str) or len(value) == 0:
+        if not isinstance(category, str) or len(category) == 0:
             raise ValueError("Category must be a non-empty string.")
-        self._category = value
+        self.category = category
+
+        self.articles = []
+        self.contributors = []
 
     def articles(self):
-        return self._articles
+        return self.articles
 
-    def contributors(self):
-        return list(set(article.author for article in self._articles))
+    def add_article(self, article):
+        if article.magazine != self:
+            raise ValueError("The specified article is not associated with this magazine.")
+        self.articles.append(article)
+        article.magazine = self
 
     def article_titles(self):
-        if not self._articles:
-            return None
-        return [article.title for article in self._articles]
+        return [article.title for article in self.articles]
 
     def contributing_authors(self):
-        author_count = {}
-        for article in self._articles:
-            author = article.author
-            if author not in author_count:
-                author_count[author] = 0
-            author_count[author] += 1
-        frequent_authors = [author for author, count in author_count.items() if count > 2]
-        return frequent_authors if frequent_authors else None
+        return [author.name for author in self.contributors]
+    
+mike = Author('Mike')
 
-    @classmethod
-    def top_publisher(cls):
-        if not cls._all_magazines:
-            return None
-        return max(cls._all_magazines, key=lambda magazine: len(magazine.articles()))
-
-
-author1 = Author("Jashon Michael")
-magazine1 = Magazine("Tech Today", "Technology")
-magazine2 = Magazine("Health Weekly", "Health")
-
-article1 = author1.add_article(magazine1, "AI in 2024")
-article2 = author1.add_article(magazine2, "Healthy Living Tips")
-
-print("Author's Articles:", [article.title for article in author1.articles()])
-print("Magazines by Author:", [mag.name for mag in author1.magazines()])
-print("Author's Topic Areas:", author1.topic_areas())
-
-print("Magazine1 Articles:", magazine1.article_titles())
-print("Magazine1 Contributors:", [author.name for author in magazine1.contributors()])
-print("Top Publisher:", Magazine.top_publisher().name)
+print (mike.name)
